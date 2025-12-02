da4d87181f2bcff6428ef9071aebc274133a59e1
parkjieun87 <dksxlzhd0807@naver.com>
2025-12-02 15:53:55 +0900

    Users테이블에 email 인덱스 추가

--- src/main/java/com/moviego/entity/Users.java
+++ src/main/java/com/moviego/entity/Users.java
@@ -7,7 +7,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name = "users")
+@Table(
+        name = "users",
+        indexes = {
+                @Index(name="idx_email", columnList = "email")
+        }
+)
 @Getter
 @Setter
 @NoArgsConstructor
