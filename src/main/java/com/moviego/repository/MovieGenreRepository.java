cbf356a99db15e3ac241ac565e4bdc4dc3f4a39f
parkjieun87 <dksxlzhd0807@naver.com>
2025-12-16 14:12:14 +0900

    Set 비교를 통한 차분 업데이트(장르)

--- src/main/java/com/moviego/repository/MovieGenreRepository.java
+++ src/main/java/com/moviego/repository/MovieGenreRepository.java
@@ -1,15 +1,21 @@
 package com.moviego.repository;
 
 import com.moviego.entity.MovieGenre;
-import io.lettuce.core.dynamic.annotation.Param;
+import com.moviego.entity.MovieGenreId;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface MovieGenreRepository extends JpaRepository<MovieGenre, Long> {
+import java.util.List;
+
+public interface MovieGenreRepository extends JpaRepository<MovieGenre, MovieGenreId> {
     @Modifying // SELECT가 아닌 INSERT, UPDATE, DELETE 쿼리임을 명시
     @Transactional // 삭제 작업이 트랜잭션 내에서 실행되도록 보장
     @Query("DELETE FROM MovieGenre mg WHERE mg.movie.movieId = :movieId")
     void deleteByMovieId(@Param("movieId") Long movieId);
+
+    @Query("SELECT mg FROM MovieGenre mg JOIN FETCH mg.genre WHERE mg.movie.movieId = :movieId")
+    List<MovieGenre> findByMovieId(@Param("movieId") Long movieId);
 }
