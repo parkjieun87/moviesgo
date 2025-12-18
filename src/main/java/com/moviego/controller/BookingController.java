7704c60ecb1d4cec73195cc51f78fbb0846aaec0
parkjieun87 <dksxlzhd0807@naver.com>
2025-12-18 14:00:23 +0900

    어노테이션 변경

--- src/main/java/com/moviego/controller/BookingController.java
+++ src/main/java/com/moviego/controller/BookingController.java
@@ -5,6 +5,7 @@ import com.moviego.service.BookingService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,7 +19,7 @@ public class BookingController {
     private final BookingService bookingService;
     // 결제 없이 바로 예매를 생성하는 API
     @PostMapping("/create")
-    public ResponseEntity<String> createBooking(@Valid @RequestBody ReservationRequest request) {
+    public ResponseEntity<String> createBooking(@Valid @ParameterObject ReservationRequest request) {
         bookingService.completeBooking(request);
         return ResponseEntity.status(HttpStatus.CREATED).body("예매가 완료되었습니다.");
     }
